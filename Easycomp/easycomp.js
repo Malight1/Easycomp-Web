2b93da719d0caf4b70ab326a363329d9b6a20139
Mubarak Oguntunde <92927004+Malight1@users.noreply.github.com>
2023-01-15 19:18:51 +0100

    Add files via upload

---
+++ Easycomp/easycomp.js
@@ -0,0 +1,13 @@
+ScrollReveal({ 
+    reset: false,
+    distance: '20px',
+    duration: 2500,
+    delay: 400
+
+});
+
+
+
+ScrollReveal().reveal('.heading, .pr, .ser' , { delay: 500, origin: 'left '});
+ScrollReveal().reveal('.page, .choose, .imgdiv, .imgdiv1, .imgd', { delay: 600, origin: 'bottom'});  
+ScrollReveal().reveal('.fat, .main, .sub',  { delay: 500, origin: 'right'});
